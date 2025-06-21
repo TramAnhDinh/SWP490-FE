2b9bd336286ef88e07a7939132c1e1592a7deb38
TramAnhDinh <dinhnguyentramanh123@gmail.com>
2025-06-21 07:14:29 +0700

    bang gia xe

--- src/pages/PricingClass.jsx
+++ src/pages/PricingClass.jsx
@@ -4,23 +4,26 @@ const PricingClass = () => {
     return (
       <div className="bg-yellow-100 p-4">
         <div className="max-w-6xl mx-auto">
-          <h2 className="text-2xl font-bold mb-4 text-center">Bảng Giá Áo Lớp</h2>
+          <h2 className="text-2xl font-bold mb-4 text-center">Bảng Giá Xe Hơi</h2>
           <div className="overflow-x-auto">
             <table className="table-auto w-full mb-8">
               <thead>
                 <tr className="bg-yellow-200">
-                  <th className="p-2 border border-white">Số lượng</th>
-                  <th className="p-2 border border-white">100% Cotton 4c (190gsm)</th>
-                  <th className="p-2 border border-white">100% Cotton 2c (250gsm)</th>
-                  <th className="p-2 border border-white">100% Cotton 4c (240gsm)</th>
+                  <th className="p-2 border border-white">Loại kính</th>
+                  <th className="p-2 border border-white">Cao cấp</th>
+                  <th className="p-2 border border-white">Siêu cao cấp</th>
+                  <th className="p-2 border border-white">Siêu đặc biệt</th>
                 </tr>
               </thead>
               <tbody>
                 {[
-                  ["21-30", "160.000", "210.000", "235.000"],
-                  ["31-40", "150.000", "190.000", "205.000"],
-                  ["41-50", "140.000", "180.000", "195.000"],
-                  ["51-60", "130.000", "170.000", "185.000"]
+                  ["Kính lái", "N 90", "ICE 90", "ICE 90"],
+                  ["Kính sườn trước","N 30 / N 20", "N 15", "ICE 40 / ICE 20"],
+                  ["Kính sườn giữa","N 30 / N 20", "N 15", "ICE 40 / ICE 20"],
+                  ["Kính sườn sau","N 30 / N 20", "N 15", "ICE 40 / ICE 20"],
+                  ["SEDAN","N 30 / N 20", "N 15", "ICE 40 / ICE 20"],
+                  ["Kính lưng","5.500.000", "8.000.000", "11.000.000"],
+                  ["SUV","6.000.000", "9.000.000", "12.000.000"]
                 ].map((row, index) => (
                   <tr key={index} className="odd:bg-gray-200">
                     {row.map((cell, i) => (
@@ -31,7 +34,7 @@ const PricingClass = () => {
               </tbody>
             </table>
   
-            <h2 className="text-2xl font-bold mb-4 text-center">Bảng Giá Áo Polo Basic</h2>
+            {/* <h2 className="text-2xl font-bold mb-4 text-center">Bảng Giá Áo Polo Basic</h2>
             <table className="table-auto w-full">
               <thead>
                 <tr className="bg-yellow-200">
@@ -56,11 +59,11 @@ const PricingClass = () => {
                   </tr>
                 ))}
               </tbody>
-            </table>
-          </div>
-        </div>
+            </table> */}
+       </div>
       </div>
-    );
+    </div>
+   )
   };
   
   export default PricingClass;
